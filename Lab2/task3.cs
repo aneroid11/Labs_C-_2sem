545b5c10e7778a9122abd1c61643cc5121be7d9a
Anton Yermakov <73517902+aneroid11@users.noreply.github.com>
2021-03-04 21:35:15 +0300

    Update task3.cs

--- Lab2/task3.cs
+++ Lab2/task3.cs
@@ -7,40 +7,61 @@ namespace Task3
 {
   class Program
   {
-    private static UInt64 MultFromAToB(UInt64 a, UInt64 b)
+    private static UInt64 CalculateDividingPowerOfTwo(UInt64 number) 
     {
-      UInt64 mult = 1;
+      UInt64 pow = 1, exponent = 0;
       
-      for (UInt64 i = a; i <= b;) 
+      while (pow <= number) 
       {
-        checked
+        try
         {
-          mult *= i;
-          i++;
+          checked
+          {
+            pow *= 2;
+          }
         }
-      }
-      
-      return mult;
-    }
-    
-    private static UInt64 CalculatePowerOfTwo(UInt64 a) {
-      UInt64 pow = 1;
-      UInt64 powMax = pow;
-      
-      while (pow <= a) {
-        if (a % pow == 0)
-        {
-          powMax = pow;
-        }
-        else
+        catch
         {
           break;
         }
         
-        pow *= 2;
+        exponent++;
+        
+        if (number % pow != 0) 
+        {
+          exponent--;
+          break;
+        }
       }
       
-      return powMax;
+      return exponent;
+    }
+    
+    private static UInt64 CalculateDividingPowerOfTwoFromAToB(UInt64 a, UInt64 b) {
+      UInt64 exponent = 0;
+      
+      for (UInt64 num = a; num <= b;) 
+      {
+        UInt64 currentExp = 0;
+        
+        currentExp = CalculateDividingPowerOfTwo(num);
+       
+        exponent += currentExp;
+        
+        try
+        {
+          checked
+          {
+            num++;
+          }
+        }
+        catch
+        {
+          break;
+        }
+      }
+      
+      return exponent;
     }
     
     public static int Main(string[] args)
@@ -57,24 +78,15 @@ namespace Task3
       
       do
       {
-        Console.Write("Enter b (>= a): ");
+        Console.Write("Enter b (> a): ");
         string strB = Console.ReadLine();
-        error = !UInt64.TryParse(strB, out b) || (b < a);
+        error = !UInt64.TryParse(strB, out b) || (b <= a);
       } while (error);
       
       Console.WriteLine("a = " + a + ", b = " + b);
       
-      try
-      {
-        UInt64 product = MultFromAToB(a, b);
-        Console.WriteLine("The product of the numbers from a to b: " + product);
-        UInt64 pow = CalculatePowerOfTwo(product);
-        Console.WriteLine("The maximum power of two that divides the product: " + pow);
-      }
-      catch (OverflowException)
-      {
-        Console.WriteLine("Cannot calculate the product of the numbers from a to b: overflow");
-      }
+      UInt64 exponent = CalculateDividingPowerOfTwoFromAToB(a, b);
+      Console.WriteLine("The maximum power of two that divides the product: 2^" + exponent);
       
       return 0;
     }
