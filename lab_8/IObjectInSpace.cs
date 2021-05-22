1cd46507d47570c31145f10fe12fca75a7697f87
aneroid11 <73517902+aneroid11@users.noreply.github.com>
2021-05-22 15:38:29 +0300

    Add files via upload

---
+++ lab_8/IObjectInSpace.cs
@@ -0,0 +1,15 @@
+﻿namespace Sky
+{
+    public interface IObjectInSpace
+    {
+        double XWorld { get; set; }
+        double YWorld { get; set; }
+        double ZWorld { get; set; }
+        double Radius { get; set; }
+
+        void RotateAroundX(double phi);
+        void RotateAroundY(double phi);
+        Allegro.Color GetColor();
+        void CreateProjection(double camAngleA, double camAngleB);
+    }
+}
